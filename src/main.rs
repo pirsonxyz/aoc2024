3e45afc19f257ab50e25820910ddfe051d72cf25
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-12-01 12:38:41 -0400

    day1
    wacky solutions, but i am happy with them

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> std::io::Result<()> {
+    // Part1
+    let mut list1: Vec<i32> = Vec::new();
+    let mut list2: Vec<i32> = Vec::new();
+    let mut distance_total = 0;
+    let file = File::open("day1.txt")?;
+    let buf_reader = BufReader::new(file);
+    for line in buf_reader.lines() {
+        if let Ok(line) = line {
+            let (distance1, distance2) = line.split_once("  ").unwrap();
+            let distance1: i32 = distance1.trim().parse().unwrap();
+            let distance2: i32 = distance2.trim().parse().unwrap();
+            list1.push(distance1);
+            list2.push(distance2);
+        } else {
+            continue;
+        };
+    }
+    list1.sort();
+    list2.sort();
+    for (distance1, distance2) in list1.iter().zip(list2.iter()) {
+        let distance_between_pairs = (distance2 - distance1).abs();
+        distance_total += distance_between_pairs;
+    }
+
+    println!("{}", distance_total);
+    // Part2
+    let mut ocurrence_score: i32 = 0;
+    for n in list1 {
+        let num_of_ocurrences = count_ocurences(&list2, n);
+        if num_of_ocurrences == 0 {
+            continue;
+        }
+        let ocurrence_s = n * num_of_ocurrences as i32;
+        ocurrence_score += ocurrence_s;
+    }
+    println!("{}", ocurrence_score);
+    Ok(())
+}
+fn count_ocurences(vec: &Vec<i32>, num: i32) -> usize {
+    vec.iter().filter(|&n| *n == num).count()
+}
