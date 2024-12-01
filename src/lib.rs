2c53768a3349005a4f68bdf8b2577ce59d0c5641
Pirson Bethancourt <pirsonbethancourt039@gmail.com>
2024-12-01 13:09:28 -0400

    make count ocurrences generic

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ pub fn read_file_and_return_lines(file_name: &str) -> std::io::Result<Lines<BufR
     let file = File::open(file_name)?;
     Ok(BufReader::new(file).lines())
 }
-pub fn count_ocurences(vec: &Vec<i32>, num: i32) -> usize {
+pub fn count_ocurences<T>(vec: &Vec<T>, num: T) -> usize
+where
+    T: std::cmp::PartialEq,
+{
     vec.iter().filter(|&n| *n == num).count()
 }
